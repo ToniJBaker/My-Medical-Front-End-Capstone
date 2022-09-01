d78197b55f3b311dd9c96bef32e7ca9e41c4cd1e
Toni Baker <tonijbaker5@gmail.com>
2022-09-01 09:38:59 -0400

    mvps complete

--- src/components/prescriptions/PrescriptionEdit.js
+++ src/components/prescriptions/PrescriptionEdit.js
@@ -1,6 +1,7 @@
 import { SideNavigation } from "../nav/SideNavigation"
-import { Link, useParams } from "react-router-dom"
-import { useState } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { fetchPrescriptions, putOption } from "../ApiManager"
 
 export const PrescriptionEdit = () => {
     const [prescription, setPrescription] = useState({
@@ -9,9 +10,29 @@ export const PrescriptionEdit = () => {
         name:"",
         dosage:""
     })
-    const {prescriptionId} = useParams()
-
+    const navigate = useNavigate()
+    const {prescriptionId} = useParams() //useParams to identify the single prescription to edit
     
+    useEffect (//fetch call for initial state
+        ()=>{
+            fetchPrescriptions(`/${prescriptionId}`)
+            .then((prescriptionToEdit)=>{
+                setPrescription(prescriptionToEdit)
+            })
+        },[]
+    )
+
+    //function to submit/update all the changes to the prescription
+    const handleUpdatePrescription = (event)=>{
+        event.preventDefault()
+        fetchPrescriptions(`/${prescriptionId}`, putOption(prescription))
+        .then(()=>{
+            navigate("/prescriptions")
+        })
+    }
+
+
+    //function to save the updated property of the prescription
     const updatePrescription = (evt) => {
         const copy = {...prescription}
         copy[evt.target.id] = evt.target.value
@@ -20,19 +41,22 @@ export const PrescriptionEdit = () => {
     
     
     return (
-    <>
+    <form onSubmit={handleUpdatePrescription}>
         <h2 className="welcome">Prescriptions</h2>
         <SideNavigation/>
         <Link to="/prescriptions" className="btn btn-primary">Back</Link>
         <div className="col-md-6">
             <label htmlFor="name" className="form-label">Prescription Name</label>
-            <input onChange={updatePrescription} type="text" className="form-control" id="name"/>
+            <input value={prescription.name} onChange={updatePrescription} type="text" className="form-control" id="name"/>
         </div>
         <div className="col-md-6">
             <label htmlFor="dosage" className="form-label">Dosage</label>
-            <input onChange={updatePrescription} type="text" className="form-control" id="dosage"/>
+            <input value={prescription.dosage} onChange={updatePrescription} type="text" className="form-control" id="dosage"/>
+        </div>
+        <div className="col-12">
+            <button type="submit" className="btn btn-primary">Update</button>
         </div>
     
-    </>
+    </form>
     )
 }
